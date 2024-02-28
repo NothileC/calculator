a3c007092aa5c437a430a244d5636c4a0a577ceb
NothileC <nothilecmhlanga@outlook.com>
2024-02-28 19:35:27 +0800

    updated javascript

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ function concatenate(num){
     document.getElementById("display").value = document.getElementById("display").value + num;
 }
 
-function clear(){
-    document.getElementById("display").value = document.getElementById("display").value('');
-}
-
  function deleteNumber(){
     document.getElementById("display").value = document.getElementById("display").value.toString().slice(0,-1);
  }
