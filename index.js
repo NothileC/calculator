45924ee78f815209c4e6d826492656fd642d484f
NothileC <136041284+NothileC@users.noreply.github.com>
2024-03-12 12:46:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+//function to calculate the sum of the numbers 
 function calculate(){
     let result = eval(document.getElementById("display").value);
     document.getElementById("display").value = result;
 }
 
+//function that joins character strings end-to-end
 function concatenate(num){
     document.getElementById("display").value = document.getElementById("display").value + num;
 }
-
+//delete button function
  function deleteNumber(){
     document.getElementById("display").value = document.getElementById("display").value.toString().slice(0,-1);
  }
